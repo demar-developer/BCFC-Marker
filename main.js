166ecf81c50a0b9e01f250902b44d6f67ddd1635
Demar Swack <demarswack@Demars-MacBook-Pro-2.local>
2018-04-08 01:28:56 -0400

    Adds auto updating

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const {app, ipcMain} = require('electron');
 const mainWindow = require('./mainWindow');
 const readItem = require('./readItem');
+const updater = require('./updater');
 
 // Listen for new read item
 ipcMain.on('new-item', (e, itemRUL) => {
@@ -16,20 +17,26 @@ ipcMain.on('new-item', (e, itemRUL) => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', mainWindow.createWindow)
+app.on('ready', () => {
+    // Create main window
+    mainWindow.createWindow();
+
+    //Check for update after x seconds
+    setTimeout( updater.check, 2000);
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') app.quit()
-})
+});
 
 app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) mainWindow.createWindow()
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
